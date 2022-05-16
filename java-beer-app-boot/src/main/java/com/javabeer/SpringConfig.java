67f906fd7b81ba619431a2450799e6637e639040
Nenad Jankovski <nenad.jankovski@scalefocus.com>
2022-05-16 08:46:28 +0200

    Added swagger support to java-beer-app-boot

--- java-beer-app-boot/src/main/java/com/javabeer/SpringConfig.java
+++ java-beer-app-boot/src/main/java/com/javabeer/SpringConfig.java
@@ -9,8 +9,13 @@ import com.javabeer.usecase.port.IdGenerator;
 import com.javabeer.usecase.port.persistance.BeerRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SpringConfig {
 
     @Bean
@@ -36,5 +41,13 @@ public class SpringConfig {
     @Bean
     public BeerController createBeerController(CreateBeer createBeer, FindBeerById findBeerById, FindBeersByCategory findBeersByCategory) {
         return new BeerController(createBeer, findBeerById, findBeersByCategory);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.javabeer.adapter.web"))
+                .build();
     }
 }
