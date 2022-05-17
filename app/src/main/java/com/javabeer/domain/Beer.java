5db8aca5341d9f2508a047f4b691afc05f5968b9
Nenad Jankovski <nenad.jankovski@scalefocus.com>
2022-05-17 16:32:23 +0200

    Added arch unit tests

--- app/src/main/java/com/javabeer/domain/Beer.java
+++ app/src/main/java/com/javabeer/domain/Beer.java
@@ -8,7 +8,6 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 public class Beer {
-
     private BeerId id;
     private String name;
     private String producer;
---
+++ app/src/test/java/com/javabeer/ArchTest.java
@@ -0,0 +1,27 @@
+package com.javabeer;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SliceRule;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+
+@AnalyzeClasses(packages = "com.javabeer")
+public class ArchTest {
+
+    @com.tngtech.archunit.junit.ArchTest
+    final ArchRule coreDomainClassesShouldNotHaveAnyExternalDependencies = ArchRuleDefinition
+            .noClasses()
+            .that()
+            .resideInAnyPackage("..domain..", "..usecase..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..adapter..");
+
+    @com.tngtech.archunit.junit.ArchTest
+    final SliceRule noCyclicDependenciesAllowedInUseCases = SlicesRuleDefinition
+            .slices()
+            .matching("com.javabeer.usecase.(*)..")
+            .should()
+            .beFreeOfCycles();
+}
