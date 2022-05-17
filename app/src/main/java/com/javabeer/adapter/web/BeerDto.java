81c4a0279fd2082551aeaa26596e2f8717008b05
Nenad Jankovski <nenad.jankovski@scalefocus.com>
2022-05-17 09:07:49 +0200

    Convert BeerDto to java record

--- app/src/main/java/com/javabeer/adapter/web/BeerDto.java
+++ app/src/main/java/com/javabeer/adapter/web/BeerDto.java
@@ -3,29 +3,12 @@ package com.javabeer.adapter.web;
 import com.javabeer.domain.Beer;
 import com.javabeer.domain.BeerCategory;
 import com.javabeer.domain.BeerId;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class BeerDto implements Serializable {
-
-    private String id;
-    private String name;
-    private String producer;
-    private String category;
+public record BeerDto(String id, String name, String producer, String category) {
 
     public BeerDto(Beer beer) {
-        this.id = beer.getId().getId();
-        this.name = beer.getName();
-        this.producer = beer.getProducer();
-        this.category = beer.getCategory().name();
+        this(beer.getId().getId(), beer.getName(), beer.getProducer(), beer.getCategory().name());
     }
-
     public Beer toBeer() {
         return Beer.builder()
                 .id(new BeerId(id))
