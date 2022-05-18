4f2cc108390834b44725ef6e11d42f4d1d503e71
Nenad Jankovski <nenad.jankovski@scalefocus.com>
2022-05-18 09:15:48 +0200

    ArchUnit tests fixed

--- app/src/test/java/com/javabeer/ArchTest.java
+++ app/src/test/java/com/javabeer/ArchTest.java
@@ -1,12 +1,13 @@
 package com.javabeer;
 
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import com.tngtech.archunit.library.dependencies.SliceRule;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 
-@AnalyzeClasses(packages = "com.javabeer")
+@AnalyzeClasses(packages = "com.javabeer", importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchTest {
 
     @com.tngtech.archunit.junit.ArchTest
@@ -19,16 +20,9 @@ public class ArchTest {
             .resideInAnyPackage("..adapter..");
 
     @com.tngtech.archunit.junit.ArchTest
-    final SliceRule noCyclicDependenciesAllowedInDomain = SlicesRuleDefinition
+    final SliceRule noCyclicDependenciesAllowed = SlicesRuleDefinition
             .slices()
-            .matching("com.javabeer.domain.(*)..")
-            .should()
-            .beFreeOfCycles();
-
-    @com.tngtech.archunit.junit.ArchTest
-    final SliceRule noCyclicDependenciesAllowedInUseCases = SlicesRuleDefinition
-            .slices()
-            .matching("com.javabeer.usecase.(*)..")
+            .matching("com.javabeer.(*)..")
             .should()
             .beFreeOfCycles();
 }
